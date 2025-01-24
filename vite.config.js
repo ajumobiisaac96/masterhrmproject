a7ec1d0c903566018b75d49017e6b1e90527f7f0
Ajumobi isaac <ajumobiisaac96@gmail.com>
2025-01-24 15:19:28 +0100

    Update paths and Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: './', // Use relative base path
+  base: '/', // Use absolute base path
   build: {
     outDir: 'dist'
   },
